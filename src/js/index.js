553c121a6f1aff466a4ed927d31137bf115cfe8e
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-04-05 21:51:02 +0100

    Setting up workflow

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
 // Global app controller
+import num from './test';
+const x = 23;
+console.log(`I imported ${num} from another module called test! Variable x is ${x}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundel.js',
-  },
-  mode: 'development'
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
