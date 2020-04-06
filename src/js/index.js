abac15f8f4b44d9ad1686088bc11c3f4eb1622e4
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-04-06 17:45:19 +0100

    Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test! Variable x is ${x}`);
+import Search from './models/Search';
+
+const search = new Search('pizza');
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults(query) {
+    try {
+      const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+      this.result = res.data.recipes;
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
+
+
